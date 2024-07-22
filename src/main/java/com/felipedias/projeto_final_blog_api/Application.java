6db2f37100e2e8dea469e07ba5aeb401e5f3f40a
Felipe <prado.dias@hotmail.com>
2024-07-22 17:00:05 -0300

    fix: correção de erro na conexão do swagger

--- src/main/java/com/felipedias/projeto_final_blog_api/Application.java
+++ src/main/java/com/felipedias/projeto_final_blog_api/Application.java
@@ -1,8 +1,11 @@
 package com.felipedias.projeto_final_blog_api;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(servers = { @Server(url = "/", description = "Default server URL")})
 @SpringBootApplication
 public class Application {
 
